dd432368c197595269c32f0666e947ea7a8676d5
Sean Shubin <sshubin@cj.com>
2019-10-03 16:14:23 -0700

    Focus on component mount

--- src/all/Bar.kt
+++ src/all/Bar.kt
@@ -5,41 +5,61 @@ import kotlinx.html.js.onChangeFunction
 import kotlinx.html.js.onClickFunction
 import kotlinx.html.js.onFocusFunction
 import org.w3c.dom.HTMLInputElement
-import react.RBuilder
+import react.*
 import react.dom.a
 import react.dom.h1
 import react.dom.input
 import react.dom.p
 
-fun RBuilder.bar(sendEvent: (MyEvent) -> Unit, bar: BarState) {
-    h1 {
-        +"Bar"
+interface BarProps : RProps {
+    var sendEvent: (MyEvent) -> Unit
+    var barState: BarState
+}
+
+class Bar : RComponent<BarProps, RState>() {
+    private val focusMe: RReadableRef<HTMLInputElement> = createRef()
+
+    override fun componentDidMount() {
+        focusMe.current?.focus()
     }
-    input {
-        attrs {
-            value = bar.barString
-            onChangeFunction = { event ->
-                val target = event.target as HTMLInputElement
-                sendEvent(UpdateBarRequest(target.value))
-            }
-            onBlurFunction = { event ->
-                val target = event.target as HTMLInputElement
-                sendEvent(StoreBarRequest(target.value))
-            }
-            onFocusFunction = { event ->
-                val target = event.target as HTMLInputElement
-                target.setSelectionRange(0, target.value.length)
+
+    override fun RBuilder.render() {
+        h1 {
+            +"Bar"
+        }
+        input {
+            attrs {
+                ref = focusMe
+                value = props.barState.barString
+                onChangeFunction = { event ->
+                    val target = event.target as HTMLInputElement
+                    props.sendEvent(UpdateBarRequest(target.value))
+                }
+                onBlurFunction = { event ->
+                    val target = event.target as HTMLInputElement
+                    props.sendEvent(StoreBarRequest(target.value))
+                }
+                onFocusFunction = { event ->
+                    val target = event.target as HTMLInputElement
+                    target.setSelectionRange(0, target.value.length)
+                }
             }
         }
-    }
-    p {
-        a(href = "#") {
-            +"Foo"
-            attrs {
-                onClickFunction = {
-                    sendEvent(LoadFooRequest)
+        p {
+            a(href = "#") {
+                +"Foo"
+                attrs {
+                    onClickFunction = {
+                        props.sendEvent(LoadFooRequest)
+                    }
                 }
             }
         }
     }
+}
+
+fun RBuilder.bar(sendEvent: (MyEvent) -> Unit,
+                 barState: BarState) = child(Bar::class) {
+    attrs.sendEvent = sendEvent
+    attrs.barState = barState
 }
--- src/all/Foo.kt
+++ src/all/Foo.kt
@@ -5,41 +5,61 @@ import kotlinx.html.js.onChangeFunction
 import kotlinx.html.js.onClickFunction
 import kotlinx.html.js.onFocusFunction
 import org.w3c.dom.HTMLInputElement
-import react.RBuilder
+import react.*
 import react.dom.a
 import react.dom.h1
 import react.dom.input
 import react.dom.p
 
-fun RBuilder.foo(sendEvent: (MyEvent) -> Unit, foo: FooState) {
-    h1 {
-        +"Foo"
+interface FooProps : RProps {
+    var sendEvent: (MyEvent) -> Unit
+    var fooState: FooState
+}
+
+class Foo : RComponent<FooProps, RState>() {
+    private val focusMe: RReadableRef<HTMLInputElement> = createRef()
+
+    override fun componentDidMount() {
+        focusMe.current?.focus()
     }
-    input {
-        attrs {
-            value = foo.fooString
-            onChangeFunction = { event ->
-                val target = event.target as HTMLInputElement
-                sendEvent(UpdateFooRequest(target.value))
-            }
-            onBlurFunction = { event ->
-                val target = event.target as HTMLInputElement
-                sendEvent(StoreFooRequest(target.value))
-            }
-            onFocusFunction = { event ->
-                val target = event.target as HTMLInputElement
-                target.setSelectionRange(0, target.value.length)
+
+    override fun RBuilder.render() {
+        h1 {
+            +"Foo"
+        }
+        input {
+            attrs {
+                ref = focusMe
+                value = props.fooState.fooString
+                onChangeFunction = { event ->
+                    val target = event.target as HTMLInputElement
+                    props.sendEvent(UpdateFooRequest(target.value))
+                }
+                onBlurFunction = { event ->
+                    val target = event.target as HTMLInputElement
+                    props.sendEvent(StoreFooRequest(target.value))
+                }
+                onFocusFunction = { event ->
+                    val target = event.target as HTMLInputElement
+                    target.setSelectionRange(0, target.value.length)
+                }
             }
         }
-    }
-    p {
-        a(href = "#") {
-            +"Bar"
-            attrs {
-                onClickFunction = {
-                    sendEvent(LoadBarRequest)
+        p {
+            a(href = "#") {
+                +"Bar"
+                attrs {
+                    onClickFunction = {
+                        props.sendEvent(LoadBarRequest)
+                    }
                 }
             }
         }
     }
+}
+
+fun RBuilder.foo(sendEvent: (MyEvent) -> Unit,
+                 fooState: FooState) = child(Foo::class) {
+    attrs.sendEvent = sendEvent
+    attrs.fooState = fooState
 }
